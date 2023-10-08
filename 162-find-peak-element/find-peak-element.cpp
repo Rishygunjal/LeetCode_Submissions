bb10498673f289e969b70e4a795fe03929905589
Rishygunjal <81767397+Rishygunjal@users.noreply.github.com>
2023-10-08 09:38:53 +0530

    Time: 6 ms (33.33%) | Memory: 9.3 MB (12.13%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -10,22 +10,21 @@ public:
             int mid = l + (r-l)/2;
             if(nums[mid-1]<=nums[mid] && nums[mid]>=nums[mid+1]) return mid-1;
             else if(nums[mid-1]<nums[mid] && nums[mid]<nums[mid+1]){
-                if(nums[l]>=nums[mid] && mid!=l){
-                    r = mid-1;
-                }else if(nums[r]<nums[mid]){
+                // if(nums[l]>=nums[mid] && mid!=l){
+                //     r = mid-1;
+                // }else if(nums[r]<nums[mid]){
                     l = mid+1;
-                }else{
-                    l = mid+1;
-                }
+                // }else{
+                //     l = mid+1;
+                // }
             }
             else if(nums[mid-1]>nums[mid] && nums[mid]>nums[mid+1]){
-                if(nums[r]>=nums[mid]){
-                    l = mid+1;
-                }else {
-                    r = mid-1;
-                } 
+                // if(nums[r]>=nums[mid]){
+                //     l = mid+1;
+                // }else {
+                    r = mid;
             }else{
-                l=mid+1;
+                    l=mid+1;
             }
         }
 
