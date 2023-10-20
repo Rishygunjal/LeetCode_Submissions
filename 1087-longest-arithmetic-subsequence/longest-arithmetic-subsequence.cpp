ac88d6e95c19b8ed0d155bbd4b109319a22f392f
Rishygunjal <81767397+Rishygunjal@users.noreply.github.com>
2023-10-20 23:39:23 +0530

    Time: 284 ms (73.99%) | Memory: 273.8 MB (17.26%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -1,21 +1,31 @@
 class Solution {
+
+int solve(vector<int> nums, int i, int diff){
+    if(i==nums.size()-1) return 0;
+
+    int ans=0;
+    for(int j=i+1;j<nums.size();j++){
+        cout<<diff<<nums[j]-nums[i]<<i<<j<<endl;
+        if(nums[j]-nums[i]==diff) ans = max(ans,1 + solve(nums,j,diff));
+        else ans = max(ans,2 + solve(nums,j,nums[j]-nums[i])); 
+    }
+    return ans; 
+}
+
 public:
-    int longestArithSeqLength(vector<int>& A) {
-        int n=A.size(),ans=0;
-        int maxval=0;
-        for(int a:A)
-            maxval=max(a,maxval);
-        vector<vector<int>> dp(2*maxval+1,vector<int>(n,0));
-        
-        for(int i=1;i<n;++i)
-            for(int j=0;j<i;++j)
-            {
-                int dif=A[i]-A[j]+maxval;
-                dp[dif][i]=dp[dif][j]+1;
-                ans=max(ans,dp[dif][i]);
-                
+    int longestArithSeqLength(vector<int>& nums) {
+        // return solve(nums,0,INT_MAX);
+        int n=nums.size(),ans=0;
+        vector<vector<int>> dp(nums.size()+1,vector<int> (2000,0));
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                dp[j][nums[j]-nums[i]+1000] = 1 + dp[i][nums[j]-nums[i]+1000];
+                ans = max(ans,dp[j][nums[j]-nums[i]+1000]);
             }
+        }
 
         return ans+1;
+        
     }
 };
