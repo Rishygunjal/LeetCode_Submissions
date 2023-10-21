f59e91087a24098c89a7b0471b60db6a015f2d5c
Rishygunjal <81767397+Rishygunjal@users.noreply.github.com>
2023-10-21 18:35:38 +0530

    Time: 590 ms (11.23%) | Memory: 218.3 MB (13.56%) - LeetSync

---
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+
+int calMaxProfit(vector<int>& prices, int i, bool hold, int ctr){
+    if(i>=prices.size() || ctr==0) return 0;
+
+    if(!hold){
+        int buy = - prices[i] + calMaxProfit(prices,i+1,1,ctr);
+        int skip = calMaxProfit(prices,i+1,0,ctr);
+        return max(buy,skip);
+    }else{
+        int sell = prices[i] + calMaxProfit(prices,i+1,0,ctr-1);
+        int skip = calMaxProfit(prices,i+1,1,ctr);
+        return max(sell,skip);
+    }
+}
+
+int calMaxProfitMem(vector<int>& prices, int i, bool hold, int ctr, vector<vector<vector<int>>>& dp){
+    if(i>=prices.size() || ctr==0) return 0;
+
+    if(dp[i][hold][ctr]!=-1) return dp[i][hold][ctr];
+
+    if(!hold){
+        int buy = - prices[i] + calMaxProfitMem(prices,i+1,1,ctr,dp);
+        int skip = calMaxProfitMem(prices,i+1,0,ctr,dp);
+        return dp[i][hold][ctr]=max(buy,skip);
+    }else{
+        int sell = prices[i] + calMaxProfitMem(prices,i+1,0,ctr-1,dp);
+        int skip = calMaxProfitMem(prices,i+1,1,ctr,dp);
+        return dp[i][hold][ctr]=max(sell,skip);
+    }
+}
+
+public:
+    int maxProfit(vector<int>& prices) {
+        // return calMaxProfit(prices,0,0,2);
+        vector<vector<vector<int>>> dp(prices.size()+1,vector<vector<int>> (2,vector<int> (3,-1)));
+        return calMaxProfitMem(prices,0,0,2,dp);
+    }
+};
