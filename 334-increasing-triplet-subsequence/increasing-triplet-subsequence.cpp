de1db61c7a792377a69dfeca2927f17b35a9f026
Rishygunjal <81767397+Rishygunjal@users.noreply.github.com>
2023-10-23 22:51:43 +0530

    Time: 104 ms (30.00%) | Memory: 112.1 MB (55.09%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int maxi1=INT_MIN,maxi2=INT_MIN,n=nums.size();
+        // int maxi=INT_MIN,n=nums.size();
+        // unordered_map<int,bool> m;
+        // for(int i=n-1;i>=0;i--){
+        //     if(nums[i]>=maxi) {
+        //         maxi=nums[i];
+        //         m[i]=0;
+        //     }else {
+        //         m[i]=1;
+        //     }
+        // }
+        // for(int i=0;i<)
+        int ctr=1,maxi1=INT_MIN+1,maxi2=INT_MIN,n=nums.size();
         for(int i=n-1;i>=0;i--){
             if(nums[i]>maxi1){
                 maxi1 = nums[i];
-            }
-            if(nums[i]==maxi1) continue;
-            if(nums[i]<maxi1) {
-                if(maxi2!=INT_MIN) {
-                    if(nums[i]<maxi2){
-                        return 1;
-                    }else maxi2 = nums[i]; 
-                }
-                else {
-                    maxi2 = nums[i];
-                }
+                // maxi2 = maxi1;
+            }else if(nums[i]==maxi1){
+                continue;
+            }else if(nums[i]>=maxi2){
+                maxi2 = nums[i];
+            }else{
+                return 1;
             }
         }
         return 0;
