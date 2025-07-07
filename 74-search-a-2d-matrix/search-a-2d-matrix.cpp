5a1963d9a41a476022b580fc8f09526e931662c6
Rishygunjal <81767397+Rishygunjal@users.noreply.github.com>
2025-07-07 10:44:00 -0700

    Time: 0 ms (100.00%) | Memory: 13.3 MB (42.06%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,28 +1,46 @@
 class Solution {
+
+bool condition1(vector<vector<int>>& matrix, int target, int mid){
+    return matrix[mid][0]>target;
+}
+
+bool condition2(vector<vector<int>>& matrix, int target, int mid, int v1){
+    return matrix[v1][mid]>=target;
+}
+
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size(),m=matrix[0].size(),l=0,r=n-1;
-
-        while(l<r){
-            int mid = l+ (r-l+1)/2;
-            if(matrix[mid][0]>target){
-                r = mid-1;
-            }else{
-                l = mid;
+        if(matrix[0][0]>target || matrix[matrix.size()-1][matrix[0].size()-1]<target) return 0;
+        int lo=-1,hi=matrix.size();
+        while(lo+1<hi){
+            int mid = lo + floor((hi-lo)/2);
+            cout<<hi<<" "<<lo<<" "<<mid<<endl;
+            if(condition1(matrix,target,mid)){
+                hi=mid;
+            }
+            else{
+                lo=mid;
             }
         }
-        if(matrix[r][0]==target) return 1;
-        int row = l;
-        // cout<<row<<endl;
+        int v1 = hi-1;
 
-        l=0,r=m-1;
-        while(l<r){
-            int mid = l+(r-l)/2;
-            if(matrix[row][mid]<target) l = mid+1;
-            else r = mid;
+        cout<<"v1 "<<v1<<endl;
+
+        if(v1==-1) return 0;
+
+        if(matrix[v1][matrix[0].size()-1]<target || matrix[v1][0]>target) return 0;
+
+        lo=-1,hi=matrix[0].size();
+        while(lo+1<hi){
+            int mid = lo + floor((hi-lo)/2);
+            if(condition2(matrix,target,mid,v1)){
+                hi=mid;
+            }else{
+                lo=mid;
+            }
         }
 
-        if(matrix[row][r]==target) return 1;
+        if(matrix[v1][hi]==target) return 1;
         else return 0;
     }
 };
