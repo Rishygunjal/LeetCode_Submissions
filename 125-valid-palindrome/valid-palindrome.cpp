f02a77a4716d139bea1dd25c0c70c9f8d935f978
Rishygunjal <81767397+Rishygunjal@users.noreply.github.com>
2025-08-11 14:40:13 +0000

    Time: 0 ms (100.00%) | Memory: 10.6 MB (12.80%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,18 +1,31 @@
 class Solution {
+
+bool checkPalindrome(string s){
+    int i=0,j=s.size()-1;
+    while(i<=j){
+        // cout<<s[i]<<" "<<s[j]<<endl;
+        if(s[i]!=s[j]) return 0;
+        i++;j--;
+    }
+    return 1;
+}
+
 public:
     bool isPalindrome(string s) {
-        string news;
+        string st;
         for(auto i:s){
-            if(isalnum(i)){
-                news.push_back(tolower(i));
+            if(isalnum(i)) {
+                if(i>=65 and i<=90){
+                    i+=32;
+                    // cout<<i<<"big"<<endl;
+                    st+=i;
+                }
+                else{
+                    st+=i;
+                }
             }
         }
-        int i=0,j=news.size()-1;
-        while(i<=j){
-            if(news[i]!=news[j]) return 0;
-            i++;
-            j--;
-        }
-        return 1;
+        cout<<st<<endl;
+        return checkPalindrome(st);
     }
 };
